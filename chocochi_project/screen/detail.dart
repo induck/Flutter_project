f5e1fcbafcc30e1c92b738567a2238d69e2feec1
induck <76930163+induck@users.noreply.github.com>
2022-10-25 07:05:13 +0900

    Update detail.dart

--- chocochi_project/screen/detail.dart
+++ chocochi_project/screen/detail.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:project3/screen/detail_screen_1.dart';
 
-class DetailScreen extends StatelessWidget{
-  final String sectionAnswer;
-  DetailScreen({
-    required this.sectionAnswer
-  });
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('첫 게시물 입니다.'),
+            leading: Image.network('https://i.postimg.cc/cJGVbF0x/1583463522726.png'),
+            onTap:(){
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => DetailScreen(),
+                )
+              );
+            },
+          ),
+          ListTile(
+            title: Text('첫 게시물 입니다.'),
+            leading: Image.network('https://i.postimg.cc/cJGVbF0x/1583463522726.png'),
+          ),
+          ListTile(
+            title: Text('첫 게시물 입니다.'),
+            leading: Image.network('https://i.postimg.cc/cJGVbF0x/1583463522726.png'),
+          ),
+          ListTile(
+            title: Text('첫 게시물 입니다.'),
+            leading: Image.network('https://i.postimg.cc/cJGVbF0x/1583463522726.png'),
+          ),
+        ],
+      ),
+    );
+  }
 }
