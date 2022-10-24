9317d502d1e8bed3944e2dd408daf4487c7df27b
induck <76930163+induck@users.noreply.github.com>
2022-10-25 07:05:51 +0900

    Create detail_screen_1.dart

---
+++ chocochi_project/screen/detail_screen_1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Text(
+          '첫 게시물 입니다.',
+        ),
+      ),
+    );
+  }
+}
